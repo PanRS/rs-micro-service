83938424044407fe9ab805284c1e6dcaff22a395
of2785 <panrusen@qianmi.com>
2018-08-21 10:50:10 +0800

    Initial commit

---
+++ src/main/java/com/rs/ParentApplication.java
@@ -0,0 +1,14 @@
+package com.rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentApplication.class, args);
+	}
+
+
+}
